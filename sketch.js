a738704450ea060c435d5577c587d189ddc4664e
diyaan <61695126+diyaan@users.noreply.github.com>
2020-03-03 17:11:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,39 @@
+//https://diyaan.github.io/BrickGame/
+var Engine = Matter.Engine;
+var World = Matter.World;
+var Bodies = Matter.Bodies;
+var engine; 
+var world;
+var ball;
+var paddle
+
 function setup() {
   createCanvas(400, 400);
+
+   engine = Engine.create();
+   world = engine.world;
+   ball = Bodies.circle(200 , 10 , 10);
+   paddle = Bodies.rectangle(150 , 200 , 100 , 20 , {isStatic:true} );
+   World.add(world , ball);
+   World.add(world , paddle);
+   Engine.run(engine);
 }
 
 var x=150;
 var y=150;
 function draw() {
   background(220);
-  rect(x , 300 , 100 , 20);
+  rect(paddle.position.x , paddle.position.y  , 100 , 20);
   if (keyIsDown(LEFT_ARROW) && x>5)
   {
-  	x=x-5;
+  	paddle.position.x=paddle.position.x-5;
   }
 
    if (keyIsDown(RIGHT_ARROW) && x<295)
   {
-  	x=x+5;
+  	paddle.position.x=paddle.position.x+5;
   }
 
-  circle(200 , y , 10);
+  circle(ball.position.x , ball.position.y , 10);
 
-  if (keyIsDown(UP_ARROW) && y>5)
-  {
-  	y=y-5;
-  }
-
-   if (keyIsDown(DOWN_ARROW) && y<390)
-  {
-  	y=y+5;
-  }
 }
