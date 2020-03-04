6019a18e94c036656b0c164f5446b0e9d99e94da
diyaan <61695126+diyaan@users.noreply.github.com>
2020-03-04 17:02:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,12 +8,12 @@ var ball;
 var paddle
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(418, 400);
 
    engine = Engine.create();
    world = engine.world;
-   ball = Bodies.circle(200 , 10 , 10);
-   paddle = Bodies.rectangle(150 , 200 , 100 , 20 , {isStatic:true} );
+   ball = Bodies.circle(150 , 10 , 5 , {restitution:1.5});
+   paddle = Bodies.rectangle(150 , 350 , 100 , 20 , {isStatic:true} );
    World.add(world , ball);
    World.add(world , paddle);
    Engine.run(engine);
@@ -21,8 +21,27 @@ function setup() {
 
 var x=150;
 var y=150;
+var rows=5;
+var columns=8;
+
 function draw() {
+	rectMode(CENTER);
+	ellipseMode(CENTER)
   background(220);
+  var i,j 
+
+for(i=0;i<columns;i=i+1)
+{
+
+  for (j=0 ;j<rows;j=j+1)
+
+    {
+
+        rect(27+(52*i) , 12 + (22*j) , 50 , 20)
+
+    }
+
+    }
   rect(paddle.position.x , paddle.position.y  , 100 , 20);
   if (keyIsDown(LEFT_ARROW) && x>5)
   {
@@ -36,4 +55,6 @@ function draw() {
 
   circle(ball.position.x , ball.position.y , 10);
 
+
 }
+
